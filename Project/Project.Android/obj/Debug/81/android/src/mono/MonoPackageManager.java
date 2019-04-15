3695f9e20583d6a7c0306ab4f27a2667811b0499
niemaattarian <G00346901@gmit.ie>
2019-04-16 00:27:20 +0100

    edits...

--- Project/Project.Android/obj/Debug/81/android/src/mono/MonoPackageManager.java
+++ Project/Project.Android/obj/Debug/81/android/src/mono/MonoPackageManager.java
@@ -106,6 +106,7 @@ public class MonoPackageManager {
 class MonoPackageManager_Resources {
 	public static final String[] Assemblies = new String[]{
 		/* We need to ensure that "Project.Android.dll" comes first in this list. */
+		"Project.Android.dll",
 		"FormsViewGroup.dll",
 		"Newtonsoft.Json.dll",
 		"Project.dll",
